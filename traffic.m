6c1b612b207ebafc13a1ab28f0e3d68a04264edf
NLGRF <nongrace6654@gmail.com>
2017-11-13 01:26:13 +0700

    final

--- traffic.m
+++ traffic.m
@@ -1,15 +1,53 @@
-obj = VideoReader('traffic.avi');
-%get(obj);
+trafficObj = VideoReader('traffic.avi');
+%get(trafficObj);
 %implay('traffic.avi');
 
-darkCar = rgb2gray(read(obj,71));
-
 darkCarValue = 50;
+darkCar = rgb2gray(read(trafficObj,71));
 noDarkCar = imextendedmax(darkCar, darkCarValue);
 
 sedisk = strel('disk',2);
 noSmallStructures = imopen(noDarkCar, sedisk);
 
-subplot(131); imshow(darkCar)
-subplot(132); imshow(noDarkCar)
-subplot(133); imshow(noSmallStructures)
+nframes = get(trafficObj, 'NumberOfFrames');
+I = read(trafficObj, 1);
+taggedCars = zeros([size(I,1) size(I,2) 3 nframes], class(I));
+
+for k = 1 : nframes
+    singleFrame = read(trafficObj, k);
+
+    % Convert to grayscale to do morphological processing.
+    I = rgb2gray(singleFrame);
+
+    % Remove dark cars.
+    noDarkCars = imextendedmax(I, darkCarValue);
+
+    % Remove lane markings and other non-disk shaped structures.
+    noSmallStructures = imopen(noDarkCars, sedisk);
+
+    % Remove small structures.
+    noSmallStructures = bwareaopen(noSmallStructures, 150);
+
+    % Get the area and centroid of each remaining object in the frame. The
+    % object with the largest area is the light-colored car.  Create a copy
+    % of the original frame and tag the car by changing the centroid pixel
+    % value to red.
+    taggedCars(:,:,:,k) = singleFrame;
+
+    stats = regionprops(noSmallStructures, {'Centroid','Area'});
+    if ~isempty([stats.Area])
+        areaArray = [stats.Area];
+        [junk,idx] = max(areaArray);
+        c = stats(idx).Centroid;
+        c = floor(fliplr(c));
+        width = 2;
+        row = c(1)-width:c(1)+width;
+        col = c(2)-width:c(2)+width;
+        taggedCars(row,col,1,k) = 255;
+        taggedCars(row,col,2,k) = 0;
+        taggedCars(row,col,3,k) = 0;
+    end
+end
+
+frameRate = get(trafficObj,'FrameRate');
+implay(taggedCars,frameRate);
