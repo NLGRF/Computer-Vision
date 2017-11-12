01dbdd8ab4f2738a718cf901afac9afed5808f3b
NLGRF <nongrace6654@gmail.com>
2017-11-13 01:19:10 +0700

    improvements

--- traffic.m
+++ traffic.m
@@ -1,9 +1,15 @@
 obj = VideoReader('traffic.avi');
+%get(obj);
+%implay('traffic.avi');
 
 darkCar = rgb2gray(read(obj,71));
 
 darkCarValue = 50;
 noDarkCar = imextendedmax(darkCar, darkCarValue);
 
-imshow(darkCar)
-figure, imshow(noDarkCar)
+sedisk = strel('disk',2);
+noSmallStructures = imopen(noDarkCar, sedisk);
+
+subplot(131); imshow(darkCar)
+subplot(132); imshow(noDarkCar)
+subplot(133); imshow(noSmallStructures)
